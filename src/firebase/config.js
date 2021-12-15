898c663df473687d1bd782327072d40e3612db28
Antxon Eguiguren <antxoneg@hotmail.com>
2021-12-15 17:14:37 +0100

    Firebase config (v9) && Firebase init

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { serverTimestamp } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBq9Pzf-IuAjEnn4BHq9r14VC6yi3M5X80",
+  authDomain: "mgmt-react.firebaseapp.com",
+  projectId: "mgmt-react",
+  storageBucket: "mgmt-react.appspot.com",
+  messagingSenderId: "408744038098",
+  appId: "1:408744038098:web:992141275ee311baf0e70b",
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+// init auth
+const auth = getAuth();
+
+// timestamp
+const timestamp = serverTimestamp();
+
+export { db, auth, timestamp };
