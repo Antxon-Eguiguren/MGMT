91d57f8050ef6dd11d488c65922d62be3de76be6
Antxon Eguiguren <antxoneg@hotmail.com>
2021-12-17 15:07:39 +0100

    useDocument custom hook created to fetch doc by id from firebase in real time

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/config';
+import { doc, onSnapshot } from 'firebase/firestore';
+
+export const useDocument = (c, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+    const ref = doc(db, `${c}/${id}`);
+    const unsubscribe = onSnapshot(
+      ref,
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+        setIsPending(false);
+      },
+      (err) => {
+        console.log(err);
+        setError(err.message);
+        setIsPending(false);
+      }
+    );
+
+    // unsubscribe on unmount
+    return () => unsubscribe();
+  }, [c, id]);
+
+  return { document, error, isPending };
+};
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { ProjectList } from '../../components/ProjectList/ProjectList';
+import { useCollection } from '../../hooks/useCollection';
 import './Dashboard.css';
 
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { documents: projects, error, isPending } = useCollection('projects');
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {isPending && <p>Loading projects...</p>}
+      {error && <p className="error">{error}</p>}
+      {projects && <ProjectList projects={projects} />}
+    </div>
+  );
 };
