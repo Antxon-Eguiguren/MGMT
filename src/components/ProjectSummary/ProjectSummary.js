919bd560515200a49cabb19f55577eb973a5087c
Antxon Eguiguren <antxoneg@hotmail.com>
2021-12-20 09:29:30 +0100

    Delete project functionality added

--- src/components/ProjectSummary/ProjectSummary.js
+++ src/components/ProjectSummary/ProjectSummary.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './ProjectSummary.css';
 import { Avatar } from '../Avatar/Avatar';
+import { useFirestore } from '../../hooks/useFirestore';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export const ProjectSummary = ({ project }) => {
+  const { deleteDocument } = useFirestore('projects');
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleClickComplete = () => {
+    deleteDocument(project.id);
+    navigate('/');
+  };
+
   return (
     <div>
       <div className="project-summary">
         <h2 className="page-title">{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className="due-date">Project due by {project.dueDate.toDate().toDateString()}</p>
         <p className="details">{project.details}</p>
         <h4>Project is assigned to:</h4>
@@ -18,6 +31,11 @@ export const ProjectSummary = ({ project }) => {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClickComplete}>
+          Delete project
+        </button>
+      )}
     </div>
   );
 };
